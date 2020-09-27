5b526d8a6fc89f81a8c736c63a047f30344d2a56
bsemmann <71895133+bsemmann@users.noreply.github.com>
2020-09-27 15:01:56 -0500

    Add files via upload

---
+++ ImportPlayers.java
@@ -0,0 +1,64 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class ImportPlayers {
+	public LinkedList<MlbData>[] mlbPlayers;
+	private int size;
+	
+	public ImportPlayers() {
+		  
+		try {
+			mlbPlayers = new LinkedList[100];
+			File myObj = new File("MLBStats.txt");
+			Scanner myReader = new Scanner(myObj);
+			  
+			while (myReader.hasNextLine()) { 
+				
+				addPlayer(myReader);
+			 }
+			  myReader.close();
+			  
+		 } catch (FileNotFoundException e) {
+			  System.out.println("An error occurred.");
+			  e.printStackTrace();
+	    } catch(NoSuchElementException e2) {
+	    	System.out.println("You are done parsing the file of MLB player statistics!");
+	    }
+	  }
+	
+	
+	public void addPlayer(Scanner myReader) {
+		String firstName = myReader.next();
+		
+		String lastName = myReader.next();
+
+		String teamName = myReader.next() + " " + myReader.next();
+		
+		if (!myReader.hasNextInt()) {
+			teamName = teamName + " " + myReader.next();
+		}
+
+		int num = myReader.nextInt();
+		
+		String pos = myReader.next();
+		
+		int homeRuns = myReader.nextInt();
+		
+		LinkedList list = new LinkedList();
+		int hash = Math.abs(firstName.hashCode()) % 100;
+		list.add(new MlbData(firstName, lastName, teamName, num, pos, homeRuns));
+		
+		mlbPlayers[hash] = list;
+		
+		size++;
+		
+	
+	}
+	
+	public int size() {
+		return size();
+	}
+}
---
+++ MlbData.java
@@ -0,0 +1,67 @@
+
+public class MlbData {
+	private String firstName;
+	private String lastName;
+	private String teamName;
+	private int jerseyNum;
+	private String position;
+	private int homeRuns;
+	
+	public MlbData(String fn, String lastName, String tn, int jn, String p, int hr) {
+		this.firstName = fn;
+		this.lastName = lastName;
+		this.teamName = tn;
+		this.jerseyNum = jn;
+		this.position = p;
+		this.homeRuns = hr;
+	}
+	
+	public void setFirstName(String name) {
+		this.firstName = name;
+	}
+	
+	public void setLastName(String name) {
+		this.lastName = name;
+	}
+	
+	public void setTeamtName(String name) {
+		this.teamName = name;
+	}
+	
+	public void setJerseyNum(int num) {
+		this.jerseyNum = num;
+	}
+	
+	public void setPosition(String pos) {
+		this.position = pos;
+	}
+	
+	public void setHomeRuns(int hr) {
+		this.homeRuns = hr;
+	}
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getTeamName() {
+		return teamName;
+	}
+	
+	public int getJerseyNum() {
+		return jerseyNum;
+	}
+	
+	public String getPosition() {
+		return position;
+	}
+	
+	public int getHomeRuns() {
+		return homeRuns;
+	}
+}
