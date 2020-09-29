a486a4c9abed2702386ec9f9d8e2dab5a4665293
bhuvi3131 <71096303+bhuvi3131@users.noreply.github.com>
2020-09-29 11:50:16 -0500

    Add files via upload

---
+++ inputdataset.java
@@ -0,0 +1,71 @@
+import java.io.File; 
+import java.io.FileNotFoundException;  
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Scanner; 
+
+public class inputdataset {
+  public LinkedList<datanode>[] baseballPlayers;
+  private int size;
+  
+  public inputdataset() {
+      
+    try {
+      baseballPlayers = new LinkedList[100];
+      File file = new File("data.txt");
+      Scanner data= new Scanner(file);
+        
+      while (data.hasNextLine()) { 
+        
+        addPlayer(data);
+       }
+        data.close();
+        
+     } catch (FileNotFoundException e) {
+        System.out.println("no data present");
+       
+      } catch(NoSuchElementException e2) {
+        System.out.println("no element present");
+      }
+    }
+  
+  private int arrayIndex(String key) {
+    int hashCode = Math.abs(key.hashCode());
+    hashCode = hashCode % 100;
+
+    return hashCode;
+  }
+  
+  
+  public void addPlayer(Scanner myReader) {
+    String firstName = myReader.next();
+    
+    String lastName = myReader.next();
+
+    String teamName = myReader.next() + " " + myReader.next();
+    
+    if (!myReader.hasNextInt()) {
+      teamName = teamName + " " + myReader.next();
+    }
+
+    int num = myReader.nextInt();
+    
+    String pos = myReader.next();
+    
+    int homeRuns = myReader.nextInt();
+    
+    LinkedList list = new LinkedList();
+    int hashCode = arrayIndex(lastName);
+    list.add(new datanode(firstName, lastName, teamName, num, pos, homeRuns));
+    
+    baseballPlayers[hashCode] = list;
+    
+    size++;
+    
+  
+  }
+  
+  public int size() {
+    return size();
+  }
+}
